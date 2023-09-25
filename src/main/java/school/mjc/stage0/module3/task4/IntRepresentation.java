9b8ce813c40dddda2fbafbf6b9ff5e053d85703f
Igor Chvalun <igor.chvalun@investengine.com>
2023-09-25 11:20:36 +0530

    Formula

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        //'c', 'h', 'a', 'r'
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+        int ic = c;
+        int ih = h;
+        int ia = a;
+        int ir = r;
+        System.out.println(ic);
+        System.out.println(ih);
+        System.out.println(ia);
+        System.out.println(ir);
     }
 }
